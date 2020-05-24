3129c84d3e8373448a2c6950105fef147645a138
Diogenes Fiorezi <diogenesfiorezi@hotmail.com>
2020-05-24 07:59:08 -0300

    Dashboard with users infomation

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,15 @@ import Login from "./components/auth/Login";
 import Alert from "./components/layout/Alert";
 import { loadUser } from "./actions/auth";
 import { Provider } from "react-redux";
+import AddExperience from "./components/profile-form/AddExperience";
 import store from "./store";
 import "./App.css";
+import CreateProfile from "./components/profile-form/CreateProfile";
 import setAuthToken from "./utils/setAuthToken";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import EditProfile from "./components/profile-form/EditProfile";
+import AddEducation from "./components/profile-form/AddEducation";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -34,6 +38,26 @@ function App() {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/edit-profile"
+                component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
+              />
             </Switch>
           </section>
         </Fragment>
